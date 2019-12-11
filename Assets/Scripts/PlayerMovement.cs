b811c3d9935faf0ad077d575a284753184018e13
Alejandro Marín <alpermar98@gmail.com>
2019-12-11 19:31:07 +0100

    PlayerControllerTest implemented

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Public atributes
+    public CharacterController contr;
+
+    public float speed = 12f;
+
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    // Private atributes
+    float x, z;
+    Vector3 move;
+    Vector3 vel;
+    bool isGrounded;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && vel.y < 0)
+        {
+            vel.y = -2f;
+        }
+
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        move = transform.right * x + transform.forward * z;
+
+        contr.Move(move * speed * Time.deltaTime);
+
+        vel.y += gravity * Time.deltaTime;
+
+        contr.Move(vel * Time.deltaTime);
+    }
+}
